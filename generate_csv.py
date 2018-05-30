c16955c903b5b8f414de46c34eaf75868b740492
Ezgi Akcora <ezgiakcora@gmail.com>
2018-05-30 21:31:12 +0200

    update

--- generate_csv.py
+++ generate_csv.py
@@ -15,17 +15,18 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import keras
 import scipy.misc
+from sklearn import preprocessing
 import dlib
 import cv2
 from imutils import face_utils
-
+import math
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Activation, Dropout, Flatten
-
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
+
 
 # initialize dlib's face detector and create a predictor
 detector = dlib.get_frontal_face_detector()
@@ -33,11 +34,13 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 # calculate distance vectors between each point and the center
 
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
 def detect_parts(image, filename):
 	distances = []
 	# resize the image, and convert it to grayscale
-	image = imutils.resize(image, width=500)
+	image = imutils.resize(image, width=200, height=200)
+	
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	# detect faces in the grayscale image
 	rects = detector(gray, 1)
@@ -47,14 +50,14 @@ def detect_parts(image, filename):
 		shape = predictor(gray, rect)
 		shape = face_utils.shape_to_np(shape)
 		distances = euclidean_all(shape)
-		#output = face_utils.visualize_facial_landmarks(image, shape)
+		# output = face_utils.visualize_facial_landmarks(image, shape)
 		# visualize all facial landmarks with a transparent overlay
-		#cv2.imshow("Image", output)
-		#cv2.waitKey(0)
+		# cv2.imshow("Image", output)
+		# cv2.waitKey(0)	
 	return distances
 
 def euclidean(a, b):
-    dist = np.linalg.norm(a-b)
+    dist = math.sqrt(math.pow((b[0] - a[0]), 2) + math.pow((b[1] - a[1]), 2))
     return dist 
 
 def euclidean_all(a):  # calculates distances between all 68 elements
@@ -66,26 +69,58 @@ def euclidean_all(a):  # calculates distances between all 68 elements
 			distances = distances + " " + str(dist)
 	return distances
 
-file_exists = os.path.isfile('testing.csv')
-if(file_exists == True):
-	os.remove('testing.csv')
 
-for path in glob.glob('../data/kaggle/PublicTest/*/*.png'): # assuming png
-	image = cv2.imread(path)
-	path, filename = os.path.split(path)
-	path, label = os.path.split(path)
-	
-	distances = detect_parts(image, filename)
-	if distances != []:
-		distances = np.asarray(distances)
+
+def generate_csv(dirName, csvName):
+	file_exists = os.path.isfile(csvName)
+	if(file_exists == True):
+		os.remove(csvName)
+
+	for path in glob.glob(dirName): # assuming png
+		img = cv2.imread(path)
+		path, filename = os.path.split(path)
+		path, label = os.path.split(path)
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+		# print(faces) #locations of detected faces
+
+		for (x,y,w,h) in faces:
+			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+			detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+			distances = detect_parts(detected_face, filename)
+			if distances != []:
+				distances = np.asarray(distances)
+			
+				with open(csvName, 'a', encoding='utf-8-sig') as csvfile:
+					fieldnames = ['emotion', 'vectors']
+					writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+					file_exists = os.path.isfile(csvName)
+					if not file_exists:	
+						writer.writeheader()
+					writer.writerow({'emotion': label, 'vectors': distances})
+					print (label, filename)
+			
+			cv2.imshow('img',img)
+			cv2.waitKey(1)
 		
-		with open('testing.csv', 'ab') as csvfile:
-    			fieldnames = ['emotion', 'vectors']
-    			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-			if not file_exists:	
-    				writer.writeheader()
-   			writer.writerow({'emotion': int(label), 'vectors': distances})
-			print label, filename
+
+# ---------------------------------------------------------------------------------------------------
+
+training_csvName = "trump.csv"
+training_dirName = './trump/*.jpg'
+generate_csv(training_dirName, training_csvName)
+'''
+testing_csvName = "testing_JAFFE.csv"
+testing_dirName = '../data/JAFFE/*/*'
+generate_csv(testing_dirName, testing_csvName)
+'''
+print("Success: CSV files are generated!")
+
+# ---------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
